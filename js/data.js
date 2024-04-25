93020823430d27e21daea5d87f7f063ce746cbb5
Bogdan <kovalskiy256@gmail.com>
2024-04-25 18:52:56 +0300

    Homework_23

---
+++ js/data.js
@@ -0,0 +1,35 @@
+const data = [{
+        name: 'Cars',
+        key: 'cars',
+        products: [{
+                id: 1,
+                name: 'Tesla Model S',
+                description: 'Electric luxury sedan.',
+                price: 80000,
+            },
+            {
+                id: 2,
+                name: 'BMW M5',
+                description: 'High-performance sports sedan.',
+                price: 100000,
+            },
+        ],
+    },
+    {
+        name: 'Motorcycles',
+        key: 'motorcycles',
+        products: [{
+                id: 3,
+                name: 'Ducati Panigale V4',
+                description: 'Italian superbike known for its performance and style.',
+                price: 25000,
+            },
+            {
+                id: 4,
+                name: 'Harley-Davidson Sportster Iron 883',
+                description: 'Classic cruiser motorcycle with a rugged look.',
+                price: 12000,
+            },
+        ],
+    },
+];
